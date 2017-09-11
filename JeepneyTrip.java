851ae1841fc41690376d3652869ed9f76d3a895c
joannbagarinao <jlbagarinao@up.edu.ph>
2017-09-11 22:32:20 +0800

    Update JeepneyTrip.java

--- JeepneyTrip.java
+++ JeepneyTrip.java
@@ -5,6 +5,9 @@ class JeepneyTrip {
   
   // constants
   final int BASE_KM = 5;
+  final double BASE_FARE = 7.0;
+  final double DISCOUNTED_FARE = 6.0;
+  final double EXCESS_FARE = 0.5;
   
   
   JeepneyTrip(double distance, int totalPassengers, int discountPassengers) {
@@ -13,28 +16,24 @@ class JeepneyTrip {
     this.discountPassengers = discountPassengers;
   }
   
-  double fareChange(double payment) {
-    if (distance = BASE_KM &&
-        totalPassengers = 1 &&
-        discountPassengers = 0) {   
-      return payment - 7;
-    } else
-        if (distance = BASE_KM &&
-            totalPassengers = 1 &&
-            discountPassengers = 1) {   
-      return payment - 6;
-    } else 
-       if (distance = BASE_KM &&
-           totalPassengers > 1 &&
-           discountPassengers = 0) {   
-      return payment - (7 × totalPassengers);
-    } else
-       if (distance = BASE_KM &&
-           totalPassengers > 1 &&
-           discountPassengers = 1) {   
-      return payment - (6 × totalPassengers);
-    } else {   
-      return 0;
-    }
-  }
+  double excessDistance(double distance) {
+     if (distance <= BASE_KM) {
+         return 0;
+     } else {
+         return distance - BASE_KM;
+     }
+   }
+
+  double computeFare(int totalPassengers, double fare, double distance) 
+     return totalPassengers * (fare + (excessDistance(distance) * EXCESS_FARE)) ;
+   }
+
+  double totalFare(int totalPassengers, int discountPassengers, double distance) {
+     return computeFare(totalPassengers - discountPassengers, BASE_FARE, distance)
+          + computeFare(discountPassengers, DISCOUNTED_FARE, distance);
+   }
+
+   double fareChange(double payment, int totalPassengers, int discountPassengers, double distance) {
+      return payment - totalFare(totalPassengers, discountPassengers, distance);
+   } 
 }
