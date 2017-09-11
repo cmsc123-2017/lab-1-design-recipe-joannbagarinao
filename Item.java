9eb99fa241c27a811a276ea4dbf740ba655d157d
joannbagarinao <jlbagarinao@up.edu.ph>
2017-09-11 22:50:16 +0800

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,1 +1,24 @@
+class Item {
+   double price;
+   double sale;
+  
+  Item (double price, double sale) {
+    this.price = price;
+    this.saale = sale;
+  }
+  
+  final int SALE_LIMIT 1000
 
+  double computeSalePrice(double price, double sale) {
+    return price - (sale * price);
+  }
+
+
+  double salePrice1000(double price, double sale) {
+     if (price > SALE_LIMIT) {
+       return computeSalePrice(price, sale);
+     } else {
+       return price;
+     }
+  }
+}
